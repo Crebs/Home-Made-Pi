7d212e45d8480e22bbe95973673d767ed9e714f6
Riley Crebs <rcrebs@salesforce.com>
2016-02-12 19:04:39 -0800

    take out code

--- light_it_up.rb
+++ light_it_up.rb
@@ -1,11 +1,6 @@
 require 'pi_piper'
 include PiPiper
 
-
-pins.each do |led|
-  File.open("/sys/class/gpio/unexport", "w") { |f| f.write("#{pin}") }
-end
-
 while 1 do
   pin = PiPiper::Pin.new(:pin => 11, :direction => :out)
   pin.on
