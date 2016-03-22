a1e3101462d791109a0ea15fb56543c8bede8bf0
Riley <rileycrebs@gmail.com>
2016-03-22 00:43:49 +0000

    add more pins

--- light_it_up.rb
+++ light_it_up.rb
@@ -1,9 +1,25 @@
 require 'pi_piper'
 include PiPiper
-
-pin = PiPiper::Pin.new(:pin => 11, :direction => :out)
+sleep_time = 0.0250
+pin = PiPiper::Pin.new(:pin => 22, :direction => :out)
+pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
+pin3 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+pin4 = PiPiper::Pin.new(:pin => 24, :direction => :out)
 while 1 do
   pin.on
-  sleep 3
+  sleep sleep_time
   pin.off
+  sleep sleep_time
+  pin2.on
+  sleep sleep_time
+  pin2.off
+  sleep sleep_time
+  pin3.on
+  sleep sleep_time
+  pin3.off
+  sleep sleep_time
+  pin4.on
+  sleep sleep_time
+  pin4.off
+  sleep sleep_time
 end
