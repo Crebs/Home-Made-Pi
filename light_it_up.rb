d0c791a521a80e1ed64576473de93a166f27b866
Riley Crebs <rcrebs@salesforce.com>
2016-03-21 18:26:09 -0700

    more compile issues

--- light_it_up.rb
+++ light_it_up.rb
@@ -2,29 +2,34 @@ require 'pi_piper'
 include PiPiper
 
 class PiLEDRunner
-  @pin = PiPiper::Pin.new(:pin => 22, :direction => :out)
-  @pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
-  @pin3 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  @pin4 = PiPiper::Pin.new(:pin => 24, :direction => :out)
-  sleep_time = 0.0250
+  
+  def initialize
+    @pin = PiPiper::Pin.new(:pin => 22, :direction => :out)
+    @pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
+    @pin3 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+    @pin4 = PiPiper::Pin.new(:pin => 24, :direction => :out)
+    @sleep_time = 0.0250
+  end
+  
+  
   def light_up_the_pi()
     i = 0
     while i < 60 do
-      @pin.on
+      self.pin.on
       sleep sleep_time
-      @pin.off
+      self.pin.off
       sleep sleep_time
-      @pin2.on
+      self.pin2.on
       sleep sleep_time
-      @pin2.off
+      self.pin2.off
       sleep sleep_time
-      @pin3.on
+      self.pin3.on
       sleep sleep_time
-      @pin3.off
+      self.pin3.off
       sleep sleep_time
-      @pin4.on
+      self.pin4.on
       sleep sleep_time
-      @pin4.off
+      self.pin4.off
       sleep sleep_time
       i+=1
     end
